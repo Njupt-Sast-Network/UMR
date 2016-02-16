41719122fb3cd059efb21d2c268721626b4604bb
Racheluxe <B14090617@njupt.edu.cn>
2016-02-16 15:09:35 +0800

    删除绑定和修改密码

--- application/User/Controller/ProfileController.class.php
+++ application/User/Controller/ProfileController.class.php
@@ -50,66 +50,12 @@ class ProfileController extends MemberbaseController {
 
     public function password()
     {
-        $userid = sp_get_current_userid();
-        $user = $this->users_model->where(array( "id" => $userid ))->find();
-        $this->assign($user);
         $this->display();
     }
 
-    public function password_post()
-    {
-        if (IS_POST) {
-            if (empty($_POST['old_password'])) {
-                $this->error("原始密码不能为空！");
-            }
-            if (empty($_POST['password'])) {
-                $this->error("新密码不能为空！");
-            }
-            $uid = sp_get_current_userid();
-            $admin = $this->users_model->where("id=$uid")->find();
-            $old_password = $_POST['old_password'];
-            $password = $_POST['password'];
-            if (sp_compare_password($old_password, $admin['user_pass'])) {
-                if ($_POST['password'] == $_POST['repassword']) {
-                    if (sp_compare_password($password, $admin['user_pass'])) {
-                        $this->error("新密码不能和原始密码相同！");
-                    }
-                    else {
-                        $data['user_pass'] = sp_password($password);
-                        $data['id'] = $uid;
-                        $r = $this->users_model->save($data);
-                        if ($r !== false) {
-                            $this->success("修改成功！");
-                        }
-                        else {
-                            $this->error("修改失败！");
-                        }
-                    }
-                }
-                else {
-                    $this->error("密码输入不一致！");
-                }
 
-            }
-            else {
-                $this->error("原始密码不正确！");
-            }
-        }
 
-    }
 
-    public function bang()
-    {
-        $oauth_user_model = M("OauthUser");
-        $uid = sp_get_current_userid();
-        $oauths = $oauth_user_model->where(array( "uid" => $uid ))->select();
-        $new_oauths = array();
-        foreach($oauths as $oa) {
-            $new_oauths[strtolower($oa['from'])] = $oa;
-        }
-        $this->assign("oauths", $new_oauths);
-        $this->display();
-    }
 
     public function upload_paper()
     {
