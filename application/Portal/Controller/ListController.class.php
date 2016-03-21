ea22b73e4ca5b64bbf2d43d1a0659a74c579add2
Racheluxe <B14090617@njupt.edu.cn>
2016-03-21 13:27:37 +0800

    新增404 朋友圈列表页显示出已审核未删除的文章列表

--- application/Portal/Controller/ListController.class.php
+++ application/Portal/Controller/ListController.class.php
@@ -50,23 +50,28 @@ class ListController extends HomebaseController {
 
     public function list_content()
     {
-        //TODO:改下面的代码
-        $term = sp_get_term($_GET['id']);
-
-        if (empty($term)) {
-            header('HTTP/1.1 404 Not Found');
-            header('Status:404 Not Found');
-            if (sp_template_file_exists(MODULE_NAME . "/404")) {
-                $this->display(":404");
-            }
-
-            return;
-        }
-
         $tplname = $term["list_tpl"];
         $tplname = sp_get_apphome_tpl($tplname, "list_content");
+		if (empty($tplname)) {
+			header('HTTP/1.1 404 Not Found');
+			header('Status:404 Not Found');
+			if (sp_template_file_exists(MODULE_NAME . "/404")) {
+				$this->display(":404");
+			}
+
+			return;
+		}
+		$db = M('content');
+		$db2 = 	M('term_relationships');
+		$obj = $db2 ->where('status=1')->select();
+		foreach ($obj as $item) {
+			$objid[] =$item['object_id'];
+		}
+		$map['post_status']=1;//判断文章是否审核通过；
+		$map['id'] = array('in',$objid);//判断文章是否被删除；
+		$list = $db->where($map)->order('post_date')->select();
+		$this->assign('lists_post',$list);
         $this->assign($term);
-        $this->assign('cat_id', intval($_GET['id']));
         $this->display(":$tplname");
     }
 	
